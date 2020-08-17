6b4b78d6ffbe71b42f85e883e090ff9337c1fb97
ZhangXinmin528 <zhangxinmin528@sina.com>
2020-08-17 23:31:16 +0800

    update home news list

--- lib/webview/src/main/java/com/coding/zxm/webview/X5WebviewActivity.kt
+++ lib/webview/src/main/java/com/coding/zxm/webview/X5WebviewActivity.kt
@@ -8,6 +8,7 @@ import com.coding.zxm.core.base.constants.RoutePath
 import com.coding.zxm.webview.fragment.X5WebViewFragment
 import com.coding.zxm.webview.x5.X5WebView
 import com.tencent.smtt.sdk.WebView
+import kotlinx.android.synthetic.main.activity_webview.*
 
 /**
  * Created by ZhangXinmin on 2020/7/26.
@@ -16,7 +17,6 @@ import com.tencent.smtt.sdk.WebView
 @Route(path = RoutePath.ROUTE_PATH_WEBVIEW)
 class X5WebviewActivity : BaseActivity(), X5WebView.WebViewListener {
 
-    private lateinit var x5WebView: X5WebView
     private var mIsWebViewAvailable = false
     private var mUrl: String? = null
     private var mIsPageFinished = false
@@ -38,7 +38,7 @@ class X5WebviewActivity : BaseActivity(), X5WebView.WebViewListener {
     }
 
     override fun initViews() {
-        x5WebView.setWebViewListener(this)
+        x5webview.setWebViewListener(this)
         mIsWebViewAvailable = true
     }
 
@@ -46,22 +46,22 @@ class X5WebviewActivity : BaseActivity(), X5WebView.WebViewListener {
         super.onStart()
         if (mIsWebViewAvailable && !TextUtils.isEmpty(mUrl)
         ) {
-            x5WebView?.loadUrl(mUrl)
+            x5webview.loadUrl(mUrl)
         }
     }
 
     override fun onPause() {
-        x5WebView.onPause()
+        x5webview.onPause()
         super.onPause()
     }
 
     override fun onResume() {
-        x5WebView.onResume()
+        x5webview.onResume()
         super.onResume()
     }
 
     override fun onDestroy() {
-        x5WebView.destroy()
+        x5webview.destroy()
         super.onDestroy()
     }
 
