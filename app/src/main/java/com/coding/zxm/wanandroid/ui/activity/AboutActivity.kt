14248580fc843d819c21293cbe9c36b9457406c4
ZhangXinmin <zhangxinmin528@sina.com>
2020-09-02 19:27:29 +0800

    增加关于页面

---
+++ app/src/main/java/com/coding/zxm/wanandroid/ui/activity/AboutActivity.kt
@@ -0,0 +1,21 @@
+package com.coding.zxm.wanandroid.ui.activity
+
+import com.coding.zxm.core.base.BaseActivity
+import com.coding.zxm.wanandroid.R
+
+/**
+ * Created by ZhangXinmin on 2020/7/26.
+ * Copyright (c) 2020/9/2 . All rights reserved.
+ */
+class AboutActivity : BaseActivity() {
+
+    override fun setLayoutId(): Int {
+        return R.layout.activity_about
+    }
+
+    override fun initParamsAndValues() {
+    }
+
+    override fun initViews() {
+    }
+}
